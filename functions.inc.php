6d6f4318456ab7ac9a14b446fe3ff76af61d494f
Micke Carlsson <mickecamino@gmail.com>
2009-02-06 19:06:14 +0000

    Added GPL license text

--- functions.inc.php
+++ functions.inc.php
@@ -1,4 +1,27 @@
 <?php
+//This file is part of FreePBX.
+//
+//    FreePBX is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    FreePBX is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with FreePBX.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Written by Diego Iastrubni <diego.iastrubni@xorcom.com>
+//  Copyright (C) 2005, Xorcom
+//
+//  This code is derived from ASTLinux 0.3
+//
+//  The original author of AST linux is:
+//  Kristian Kielhofner - KrisCompanies, LLC - http://astlinux.org/
+//
 
 function cli_runcommand($txtCommand) {
 	global $astman;
--- page.cli.php
+++ page.cli.php
@@ -1,31 +1,27 @@
 <?php
-/*
- *  Written by Diego Iastrubni <diego.iastrubni@xorcom.com>
- *  Copyright (C) 2005, Xorcom
- *
- *  All rights reserved.
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *  This code is derived from ASTLinux 0.3, from the file
- *  /var/www/admin/asterisk.php
- *
- *  The original author of AST linux is:
- *  Kristian Kielhofner - KrisCompanies, LLC - http://astlinux.org/
- */
-
-
+//This file is part of FreePBX.
+//
+//    FreePBX is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    FreePBX is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with FreePBX.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Written by Diego Iastrubni <diego.iastrubni@xorcom.com>
+//  Copyright (C) 2005, Xorcom
+//
+//  This code is derived from ASTLinux 0.3
+//
+//  The original author of AST linux is:
+//  Kristian Kielhofner - KrisCompanies, LLC - http://astlinux.org/
+//
 $txtCommand = isset($_POST['txtCommand'])?$_POST['txtCommand']:'';
 ?>
 
