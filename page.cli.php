278b526aad837ccee1fbcdb359e684f7fc515259
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-03 18:12:03 -0500

    Update license info

--- page.cli.php
+++ page.cli.php
@@ -1,27 +1,8 @@
 <?php
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
-//This file is part of FreePBX.
-//
-//    FreePBX is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 2 of the License, or
-//    (at your option) any later version.
-//
-//    FreePBX is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with FreePBX.  If not, see <http://www.gnu.org/licenses/>.
-//
-//  Written by Diego Iastrubni <diego.iastrubni@xorcom.com>
-//  Copyright (C) 2005, Xorcom
-//
-//  This code is derived from ASTLinux 0.3
-//
-//  The original author of AST linux is:
-//  Kristian Kielhofner - KrisCompanies, LLC - http://astlinux.org/
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//	Copyright (C) 2005, Xorcom
+//	Copyright 2013 Schmooze Com Inc.
 //
 $txtCommand = isset($_POST['txtCommand'])?$_POST['txtCommand']:'';
 
