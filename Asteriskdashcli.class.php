ec73b6724410f830377f2ed4d058adebe1eb965b
Arun NV <arunav@sangoma.com>
2023-08-10 16:28:39 +0530

    PHP 8.1 compatibility issues fixed

--- Asteriskdashcli.class.php
+++ Asteriskdashcli.class.php
@@ -2,41 +2,44 @@
 //	License for all code of this FreePBX module can be found in the license file inside the module directory
 //	Copyright 2013 Schmooze Com Inc.
 namespace FreePBX\modules;
+
 class Asteriskdashcli implements \BMO {
 	public function __construct($freepbx = null) {
 		if ($freepbx == null) {
 			throw new Exception("Not given a FreePBX Object");
 		}
 		$this->FreePBX = $freepbx;
-		$this->AstMan = $freepbx->astman;
+		$this->AstMan  = $freepbx->astman;
 	}
-	public function install() {}
-	public function uninstall() {}
-	public function backup() {}
-	public function restore($backup) {}
-	public function doConfigPageInit($page) {}
+	public function install() {
+	}
+	public function uninstall() {
+	}
+	public function backup() {
+	}
+	public function restore($backup) {
+	}
+	public function doConfigPageInit($page) {
+	}
 	public function ajaxRequest($req, &$setting) {
 		$return_data = false;
-		switch($req)
-		{
-			case "clicmd":
-			case "getCliCommands":
-				$return_data = true;
-				break;
-		}
+		$return_data = match ($req) {
+			"clicmd", "getCliCommands" => true,
+			default => $return_data,
+		};
 		return $return_data;
 	}
 
 	public function ajaxHandler() {
-		switch($_REQUEST['command']) { 
+		switch ($_REQUEST['command']) {
 			case "clicmd":
 				$res = $this->cli_runcommand($_REQUEST['data']);
-				return json_encode($res);
+				return json_encode($res, JSON_THROW_ON_ERROR);
 				break;
 
 			case "getCliCommands":
 				$res = $this->cli_getcommands(true);
-				return array('status' => true, 'cliCommands' => $res);
+				return [ 'status' => true, 'cliCommands' => $res ];
 				break;
 		}
 	}
@@ -52,14 +55,15 @@ class Asteriskdashcli implements \BMO {
 	 */
 	public function cli_runcommand($txtCommand) {
 		if ($this->AstMan) {
-			$response = $this->AstMan->send_request('Command',array('Command'=>"$txtCommand"));
-			if(!empty($response['data'])) {
-				$response = explode("\n",$response['data']);
+			$response = $this->AstMan->send_request('Command', [ 'Command' => "$txtCommand" ]);
+			if (!empty($response['data'])) {
+				$response = explode("\n", (string) $response['data']);
 				unset($response[0]); //remove the Priviledge Command line
-				$response = implode("\n",$response);
+				$response = implode("\n", $response);
 				$html_out = htmlspecialchars($response);
 				return $html_out;
-			} else {
+			}
+			else {
 				return _("No Output Returned");
 			}
 
@@ -67,19 +71,19 @@ class Asteriskdashcli implements \BMO {
 	}
 
 	public function cli_getcommands($info = false) {
-		$return_data = array();
+		$return_data = [];
 		if ($this->AstMan) {
-			$response = $this->AstMan->send_request('Command',array('Command'=>"core show help"));
-			if(!empty($response['data'])) {
-				$response = explode("\n",$response['data']);
+			$response = $this->AstMan->send_request('Command', [ 'Command' => "core show help" ]);
+			if (!empty($response['data'])) {
+				$response = explode("\n", (string) $response['data']);
 				unset($response[0]); //remove the Priviledge Command line
 
-				foreach($response as $line) {
-					if (strlen((trim($line))) == 0) { continue; }
+				foreach ($response as $line) {
+					if (strlen((trim($line))) == 0) {
+						continue;
+					}
 					$help_cmd = $cmd = explode("--", $line);
-					$add_cmd = array(
-						'cmd' => trim($help_cmd[0]),
-					);
+					$add_cmd = [ 'cmd' => trim($help_cmd[0]) ];
 					if ($info) {
 						$add_cmd['info'] = trim(trim($help_cmd[1]));
 					}
